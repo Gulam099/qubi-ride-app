609b50e8bf743eb1a1f9308e9ff779db8dfbcc32
unknown <razag5537@gmail.com>
2025-07-03 19:11:37 +0530

    this is the code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -48,7 +48,7 @@ import {
 } from "@clerk/clerk-expo";
 import { Text } from "@/components/ui/Text";
 import { tokenCache } from "@/lib/cache";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/queryClient";
 
@@ -366,18 +366,20 @@ const AppContent = () => {
     <ThemeProvider>
       <GestureHandlerRootView>
         <SafeAreaProvider>
-          <Stack>
-            <Stack.Screen name="+not-found" />
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="(stacks)" options={{ headerShown: false }} />
-            <Stack.Screen name="(modals)" options={{ headerShown: false }} />
-            <Stack.Screen name="consult" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="instant-booking"
-              options={{ headerShown: false }}
-            />
-          </Stack>
+          <SafeAreaView style={{ flex: 1 }}>
+            <Stack>
+              <Stack.Screen name="+not-found" />
+              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="(stacks)" options={{ headerShown: false }} />
+              <Stack.Screen name="(modals)" options={{ headerShown: false }} />
+              <Stack.Screen name="consult" options={{ headerShown: false }} />
+              <Stack.Screen
+                name="instant-booking"
+                options={{ headerShown: false }}
+              />
+            </Stack>
+          </SafeAreaView>
         </SafeAreaProvider>
         <Toaster position="top-center" />
         <StatusBar style="light" backgroundColor={colors.blue[700]} />
